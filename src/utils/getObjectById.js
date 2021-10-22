e6ed8069842937dfe6f82d53d75a510090db3295
Eugene <eugen1mn@gmail.com>
2021-10-22 15:52:07 +0300

    Refactor  getObjectById

--- src/utils/getObjectById.js
+++ src/utils/getObjectById.js
@@ -1,7 +1,6 @@
 const getObjectById = (arr, id) => {
-  const copyArr = arr.slice();
-  const index = copyArr.findIndex((item) => item._id === id);
-  return { ...copyArr.splice(index, 1)[0] };
+  const obj = arr.filter((item) => item._id === id)[0];
+  return { ...obj };
 };
 
 export default getObjectById;
